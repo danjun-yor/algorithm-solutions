5f08e926ca96095e651ffeca8fe313eb181d350a
danjun <chljy33@naver.com>
2019-09-06 22:02:56 +0900

    [프로그래머스][동적계획법]정수삼각형

--- 동적계획법/정수삼각형.js
+++ 동적계획법/정수삼각형.js
@@ -1,30 +1,24 @@
 (() => {
   function solution(triangle) {
     const cache = new Array(triangle.length)
-      .fill(null)
-      .map((v, i) => new Array(i + 1).fill(null));
-    console.log(cache);
-    dp(0, 0, 0);
-    function dp(x, y, sum) {
-      if (x >= triangle.length) {
-        return;
-      }
-      console.log(x, y, sum);
-      if (x === 0 && y === 0) {
-        cache[x][y] = triangle[x][y];
-        sum = cache[x][y];
-      } else {
-        if (cache[x][y]) {
-          cache[x][y] = Math.max(sum + triangle[x][y], cache[x][y]);
+      .fill(0)
+      .map((v, i) => new Array(i + 1).fill(0));
+    cache[0][0] = triangle[0][0];
+    cache[1][0] = cache[0][0] + triangle[1][0];
+    cache[1][1] = cache[0][0] + triangle[1][1];
+
+    for (var i = 2; i < triangle.length; i++) {
+      for (var j = 0; j <= i; j++) {
+        if (j === 0) {
+          cache[i][j] = cache[i - 1][j] + triangle[i][j];
+        } else if (j === i) {
+          cache[i][j] = cache[i - 1][j - 1] + triangle[i][j];
         } else {
-          cache[x][y] = sum + triangle[x][y];
+          cache[i][j] =
+            Math.max(cache[i - 1][j - 1], cache[i - 1][j]) + triangle[i][j];
         }
-        sum = cache[x][y];
       }
-      dp(x + 1, y, sum);
-      dp(x + 1, y + 1, sum);
     }
-    console.log(cache);
     return Math.max(...cache[triangle.length - 1]);
   }
   let triangle = [[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]];
