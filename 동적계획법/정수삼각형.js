7ed00e37bfd2f3b9a8280bae219cdefb08a8d808
danjun <chljy33@naver.com>
2019-09-06 22:04:47 +0900

    주석추가

--- 동적계획법/정수삼각형.js
+++ 동적계획법/정수삼각형.js
@@ -3,6 +3,8 @@
     const cache = new Array(triangle.length)
       .fill(0)
       .map((v, i) => new Array(i + 1).fill(0));
+
+    // 초기값
     cache[0][0] = triangle[0][0];
     cache[1][0] = cache[0][0] + triangle[1][0];
     cache[1][1] = cache[0][0] + triangle[1][1];
@@ -10,10 +12,13 @@
     for (var i = 2; i < triangle.length; i++) {
       for (var j = 0; j <= i; j++) {
         if (j === 0) {
+          // 첫 열은 그냥 더함
           cache[i][j] = cache[i - 1][j] + triangle[i][j];
         } else if (j === i) {
+          // 마지막 열은 그냥 더함
           cache[i][j] = cache[i - 1][j - 1] + triangle[i][j];
         } else {
+          // 비교하여 최댓값을 더함
           cache[i][j] =
             Math.max(cache[i - 1][j - 1], cache[i - 1][j]) + triangle[i][j];
         }
