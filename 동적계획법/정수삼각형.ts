7d5597814397c0d4541d86c24e3a3609a98e99df
danjun <chljy33@naver.com>
2019-09-06 21:22:40 +0900

    [프로그래머스][동적계획법]정수삼각형 1차풀이 32.1점

---
+++ 동적계획법/정수삼각형.ts
@@ -0,0 +1,33 @@
+(() => {
+  function solution(triangle: Array<Array<number>>) {
+    const cache = new Array(triangle.length)
+      .fill(null)
+      .map((v, i) => new Array(i + 1).fill(null));
+    console.log(cache);
+    dp(0, 0, 0);
+    function dp(x, y, sum) {
+      if (x >= triangle.length) {
+        return;
+      }
+      console.log(x, y, sum);
+      if (x === 0 && y === 0) {
+        cache[x][y] = triangle[x][y];
+        sum = cache[x][y];
+      } else {
+        if (cache[x][y]) {
+          cache[x][y] = Math.max(sum + triangle[x][y], cache[x][y]);
+        } else {
+          cache[x][y] = sum + triangle[x][y];
+        }
+        sum = cache[x][y];
+      }
+      dp(x + 1, y, sum);
+      dp(x + 1, y + 1, sum);
+    }
+    console.log(cache);
+    return Math.max(...cache[triangle.length - 1]);
+  }
+
+  let triangle = [[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]];
+  console.log(solution(triangle));
+})();
